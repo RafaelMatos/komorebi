4fa3462e110892ac8e72d10a07809153b25387e1
RafaelMatos <rafael-matos@outlook.com>
2024-04-30 20:25:30 -0300

    feat: add Hero component on home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/_components/shadcn/ui/button";
+import Hero from "./_component/hero";
+
+
+
+export default function Home() {
+  
+  return (
+    <div className="flex flex-col w-full h-[100vh] justify-center items-center">
+      <Hero/>
+      
+     
+    </div>
+  );
+}
